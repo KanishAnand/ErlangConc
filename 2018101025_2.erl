db70c0e93b812007e615688ef9e44addcb390d5d
KanishAnand <therealkanish@gmail.com>
2021-02-18 02:32:31 +0530

    second question almost done

--- 2018101025_2.erl
+++ 2018101025_2.erl
@@ -12,25 +12,36 @@ main(Args) ->
     Edges = [{1, 2, 1}, {1, 3, 1}, {1, 4, 3}, {2, 4, 1},
 	     {3, 4, 1}],
     Source = 1,
-    % io:format("~w  ~w ~n", [lists:nth(1, Edges), Source]),
     % initialize distance array with infinity with dis[source] = 0
     Distance = lists:duplicate(Source - 1, ?INF) ++
 		 [0] ++ lists:duplicate(N - Source, ?INF),
     % No of edges per process
     No_per_process = M div P,
-    % io:format("~w  ~n",
-    %       [lists:sublist(Edges, 1, No_per_process)]),
     Pid_lst = create_process(self(), P, M, Edges, 1,
 			     No_per_process, []),
-    % io:format("Pids ~w ~n", [Pid_lst]),
-    [Pid ! {Distance} || Pid <- Pid_lst],
-    send_distance(Distance).
+    Ans = loop(P, 0, Pid_lst, Distance),
+    io:format("Ans ~w ~n", [Ans]).
 
-send_distance(Distance) ->
+loop(P, P, _, Distance) -> Distance;
+loop(P, Cnt, Pid_lst, Distance) ->
+    [Pid ! {Distance} || Pid <- Pid_lst],
+    Final_dis = send_distance(P, 0, Distance),
+    io:format("FFFs ~w ~n", [Final_dis]),
+    loop(P, Cnt + 1, Pid_lst, Final_dis).
+
+send_distance(P, P, Distance) -> Distance;
+send_distance(P, Cnt, Distance) ->
     receive
-      {Upd_distance} -> io:format("DS ~w ~n", [Upd_distance])
+      {Upd_distance} ->
+	  Final_dis = update(Upd_distance, Distance, [])
     end,
-    send_distance(Distance).
+    send_distance(P, Cnt + 1, Final_dis).
+
+update([], [], Final_dis) -> Final_dis;
+update(Upd_distance, Distance, Final_dis) ->
+    [Dis1 | Tail1] = Upd_distance,
+    [Dis2 | Tail2] = Distance,
+    update(Tail1, Tail2, Final_dis ++ [min(Dis1, Dis2)]).
 
 create_process(Main_pid, P, M, Edges, Start,
 	       No_per_process, Pid_lst) ->
@@ -50,13 +61,14 @@ create_process(Main_pid, P, M, Edges, Start,
     end.
 
 bellman_ford(Main_pid, Edges) ->
-    io:format("K ~w  ~n", [Edges]),
+    % io:format("K ~w  ~n", [Edges]),
     receive
       {Distance} ->
 	  %   io:format("OP ~w  ~n", [Distance]),
 	  Updated_dist = update_dist(Edges, Distance),
 	  Main_pid ! {Updated_dist}
-    end.
+    end,
+    bellman_ford(Main_pid, Edges).
 
 update_dist([], Distance) -> Distance;
 update_dist(Edges, Distance) ->
@@ -65,23 +77,14 @@ update_dist(Edges, Distance) ->
     % io:format("AA ~w ~w ~w~n", [Src, Dest, Cost]),
     A = lists:nth(Src, Distance),
     B = lists:nth(Dest, Distance),
+    % io:format("BB ~w ~w ~w ~n", [A, B, Cost]),
     if B > A + Cost ->
 	   Update_dist = lists:sublist(Distance, Dest - 1) ++
 			   [A + Cost] ++ lists:nthtail(Dest, Distance);
        A > B + Cost ->
 	   % undirected graph
 	   Update_dist = lists:sublist(Distance, Src - 1) ++
-			   [B + Cost] ++ lists:nthtail(Src, Distance)
+			   [B + Cost] ++ lists:nthtail(Src, Distance);
+       true -> Update_dist = Distance
     end,
     update_dist(Tail, Update_dist).
-
-% update_dist(Edg, Distance) ->
-%     {Src, Dest, Cost} = Edg,
-%     A = lists:nth(Src, Distance),
-%     B = lists:nth(Dest, Distance),
-%     if B > A + Cost ->
-% 	   io:format("~w ~w ~w~n", [A, Dest, Cost]);
-%        % undirected graph
-%        A > B + Cost -> io:format("~w ~n", [Cost])
-%     end.
-
